99ecb71617ba82fd8f9b53d71dc07ee44397afe3
leeho <leeho@DESKTOP-L00PKD4>
2023-05-05 19:30:31 -0700

    Add project files.

---
+++ Houk_OOP_Bank/Bank.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Houk_OOP_Bank
+{
+    internal class Bank
+    {
+        private decimal _balance;
+        private decimal _withdraw;
+        private decimal _deposit;
+
+        public Bank(decimal balance)
+        {
+            _balance = balance;
+        }
+
+        public decimal GetBalance()
+        {
+            return Balance;
+        }
+
+        public decimal Deposit(decimal amount)
+        {
+            DepositAmount = amount;
+            Balance += DepositAmount;
+            return Balance;
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            WithdrawAmount = amount;
+            Balance -= WithdrawAmount;
+            return Balance;          
+        }
+
+        public static string Error()
+        {
+            return "Withdrawals are limited to $500";
+        }
+
+        public decimal Balance
+        {
+            get { return _balance; }
+            set 
+            { 
+                if (value < 0)
+                    value = 0;
+                _balance = value; 
+            }            
+        }
+
+        public decimal WithdrawAmount
+        {
+            get { return _withdraw; }
+            set
+            {
+                if (value > 500)
+                    value = 500;
+                if (value < 0)
+                    value = 0;
+                _withdraw = value;
+            }
+        }
+        public decimal DepositAmount
+        {
+            get { return _deposit; }
+            set
+            {
+                if (value < 0)
+                    value = 0;
+                _deposit = value;
+            }
+        }
+    }
+}
---
+++ Houk_OOP_Bank/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+// Lee Houk
+// IT112
+// NOTES: Good first project for building a class and implementing encapsulation.
+// BEHAVIORS NOT IMPLEMENTED AND WHY: None
+
+namespace Houk_OOP_Bank
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            Bank Bank = new Bank(1000);
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = Menu(Bank);
+            }
+
+            static bool Menu(Bank Bank)
+            {
+                decimal amount;
+                
+                if (Bank.Balance > 0)
+                {
+                    Console.WriteLine("Please choose from the listed options:");
+                    Console.WriteLine("1) Make a deposit");
+                    Console.WriteLine("2) Make a withdrawal");
+                    Console.WriteLine("3) Exit");
+                    Console.WriteLine("\r\nSelect an option:");
+
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            Console.WriteLine("How much would you like to deposit?");
+                            amount = decimal.Parse(Console.ReadLine());
+                            Bank.Deposit(amount);
+                            Console.WriteLine("\nYour new balance is " + Bank.GetBalance().ToString("C") + "\n");
+                            return true;
+                        case "2":
+                            Console.WriteLine("How much would you like to withdraw?");
+                            amount = decimal.Parse(Console.ReadLine());
+                            if (amount <= 500)
+                            {
+                                Bank.Withdraw(amount);
+                                Console.WriteLine("\nYour new balance is " + Bank.GetBalance().ToString("C") + "\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\n" + Bank.Error());
+                                Bank.Withdraw(amount);
+                                Console.WriteLine("Your new balance is " + Bank.GetBalance().ToString("C") + "\n");
+                            }
+                            return true;
+                        case "3":
+                            return false;
+                        default:
+                            return true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please choose from the listed options:");
+                    Console.WriteLine("1) Make a deposit");
+                    Console.WriteLine("2) Exit");
+                    Console.WriteLine("\r\nSelect an option:");
+
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            Console.WriteLine("How much would you like to deposit?");
+                            amount = decimal.Parse(Console.ReadLine());
+                            Bank.Deposit(amount);
+                            Console.WriteLine("\nYour new balance is " + Bank.GetBalance().ToString("C") + "\n");
+                            return true;
+                        case "2":
+                            return false;                           
+                        default:
+                            return true;
+                    }
+                }
+            }                     
+        }
+    }
+}
